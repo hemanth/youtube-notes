eca407109714265dc738f215b3f19d03bf3e10fe
Hemanth.HM <hemanth.hm@gmail.com>
2014-12-21 10:28:41 +0530

    Changes as per the new design.

--- notes.js
+++ notes.js
@@ -10,19 +10,18 @@ var s = document.createElement('span');
 b.appendChild(s);
 s.innerHTML = "<b>Notes</b>";
 b.appendChild(s);
-var d = document.querySelector('div#watch7-sentiment-actions');
+d = document.querySelector('.watch-title');
 if(d){
 	d.appendChild(b);
 	var code = document.createElement('div');
 	var notes = document.createElement('textarea');
 	notes.id='yt-notes';
-	notes.style.cssText="background: url(http://i.stack.imgur.com/ynxjD.png) repeat-y ;width:600px;height:300px;line-height:25px;padding: 2px 10px;border:soild 1px #ddd;";
+	notes.style.cssText="background: url(//i.stack.imgur.com/ynxjD.png) repeat-y ;width:600px;height:300px;line-height:25px;padding: 2px 10px;border:soild 1px #ddd;";
 	notes.style.display='none';
 	code.appendChild(notes);
 	d.appendChild(code);
 	if (window.localStorage) {
-	      var new_notes = 'Save your notes here.',
-	      unsupported = 'Snap, get a good browser first!';
+	      var new_notes = 'Save your notes here.';
 	      var key = window.location.href.split("v=")[1].substring(0, 11);
 	      var p = document.querySelector('#yt-notes');
 	      if (localStorage.getItem(key) == null) {
@@ -35,6 +34,7 @@ if(d){
 	      	localStorage.setItem(key,p.value);
 	      }, false);
 	  } else {
+	  	  var unsupported = 'Snap, get a good browser first!';
 	      document.getElementById('yt-notes').value = unsupported;
 	  }
 	b.addEventListener('click', function(){
